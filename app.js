b611c5c53b52271f9ebbfdc91b192747c49c51bd
Oluwajuwon Akanbi <omokoreakanbi@gmail.com>
2022-09-28 22:18:04 +0200

    Example JavaScript program

---
+++ app.js
@@ -0,0 +1,18 @@
+const rect = {
+  parimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+function solveRect(l, w) {
+  console.log(`Solving for rectangle with dimension:${l},${w}`);
+  if (l <= 0 || w <= 0) {
+    console.log(
+      `Rectangle dimension must be greater than zero. Received: ${l},${w}`
+    );
+  } else {
+    console.log(`Area of rectangles:${rect.area(l, w)}`);
+    console.log(`Parimeter of rectangle:${rect.parimeter(l, w)}`);
+  }
+}
+
+solveRect(2, 4);
+solveRect(0, 0);
